31351d7d915b6033635d97e9beeec40f12f14d2d
Barry Burke <SANdood@users.noreply.github.com>
2015-10-26 11:48:11 -0400

    Manual update to Kristopher's latest master

--- circadian-daylight.groovy
+++ circadian-daylight.groovy
@@ -1,5 +1,5 @@
 /**
- *  Circadian Daylight
+ *  Circadian Daylight 2.0
  *
  *  This SmartApp synchronizes your color changing lights with local perceived color  
  *     temperature of the sky throughout the day.  This gives your environment a more 
@@ -14,13 +14,7 @@
  * 	hues.  Hormone production, brainwave activity, mood and wakefulness are 
  * 	just some of the cognitive functions tied to cyclical natural light.
  *	http://en.wikipedia.org/wiki/Zeitgeber
- *
- *  The SmartApp is completely dependent on "things happening" in your environment:
- *     specifically, motion or switches.  I originally intended for this app to run
- *     on a schedule, but SmartThings tends to not publish applications that have 
- *     an internal scheduler call.  If you want to hack a scheduler into this app, 
- *     just add "schedule(0 0/5 * * * ?, sunHandler)" into the initialize() routine 
- *  
+ * 
  *  Here's some further reading:
  * 
  * http://www.cambridgeincolour.com/tutorials/sunrise-sunset-calculator.htm
@@ -37,13 +31,18 @@
  *     *  There are no considerations for weather or altitude, but does use your 
  *		hub's zip code to calculate the sun position.    
  *     *  The app doesn't calculate a true "Blue Hour" -- it just sets the lights to
- *		2700K (warm white) until your hub goes into Sleep mode
+ *		2700K (warm white) until your hub goes into Night mode
  *
- *  Version 1.1: April 1, 2015 - Add support for contact sensors
+ *  Version 2.0: September 19, 2015 - Update for Hub 2.0
+ *  Version 1.5: June 26, 2015 - Merged with SANdood's optimizations, breaks unofficial LIGHTIFY support
+ *  Version 1.4: May 21, 2015 - Clean up mode handling
+ *  Version 1.3: April 8, 2015 - Reduced Hue IO, increased robustness
+ *  Version 1.2: April 7, 2015 - Add support for LIGHTIFY bulbs, dimmers and user selected "Sleep"
+ *  Version 1.1: April 1, 2015 - Add support for contact sensors 
  *  Version 1.0: March 30, 2015 - Initial release
  *  
  *  The latest version of this file can be found at
- *     https://github.com/KristopherKubicki/smartapp-circadian-daylight/blob/master/circadian-daylight.groovy
+ *     https://github.com/KristopherKubicki/smartapp-circadian-daylight/
  *   
  */
 
@@ -53,209 +52,250 @@ definition(
 	author: "kristopher@acm.org",
 	description: "Sync your color changing lights with natural daylight hues",
 	category: "Green Living",
-	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Partner/hue.png",
-	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Partner/hue@2x.png"
+	iconUrl: "https://s3.amazonaws.com/smartapp-icons/MiscHacking/mindcontrol.png",
+	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/MiscHacking/mindcontrol@2x.png"
 )
 
 preferences {
-	section("When these switches turn on/off...") {
-		input "switches", "capability.switch", title: "Which switches?", multiple:true
-	}
-	section("Or these motion sensors are activated...") {
-		input "motions", "capability.motionSensor", title: "Which motions?", multiple:true
-	}
-	section("Or these contact sensors open/close...") {
-	 	input "contacts", "capability.contactSensor", title: "Which contacts?", multiple:true
+	section("If these motion sensors are activated...") {
+		input "motions", "capability.motionSensor", title: "Which motion sensors?", multiple:true, required: false
+		input "contacts", "capability.contactSensor", title: "And/or which contact sensors?", multiple:true, required: false
 	}
 	section("Control these bulbs...") {
-		input "bulbs", "capability.colorControl", title: "Which Hue Bulbs?", multiple:true
+		input "bulbs", "capability.colorControl", title: "Which Color Changing Bulbs?", multiple:true, required: false
+		input "ctbulbs", "capability.colorTemperature", title: "Which Temperature Changing Bulbs?", multiple:true, required: false
+		input "dimmers", "capability.switchLevel", title: "Which Dimmers?", multiple:true, required: false
 	}
+    section("What are your 'Sleep' modes?") {
+		input "smodes", "mode", title: "What are your Sleep modes?", multiple:true, required: false
+	}
+    section("Enabled Dynamic Brightness?") { 
+    	input "dbright","bool", title: "Yes or no?", required: false
+    }
+    section("Enabled Campfire instead of Moonlight?") { 
+    	input "dcamp","bool", title: "Yes or no?", required: false
+    }
 }
 
+
 def installed() {
+	unsubscribe()
+    unschedule()
 	initialize()
 }
 
 def updated() {
 	unsubscribe()
-	unschedule()
+    unschedule()
 	initialize()
 }
 
-
 private def initialize() {
 	log.debug("initialize() with settings: ${settings}")
-	subscribe(switches, "switch", sunHandler)
-	subscribe(motions, "motion", sunHandler)
-	subscribe(contacts, "contact", sunHandler)
-//	schedule("0 */6 * * * ?", sunHandler)			// "gentle" polling every 6 minutes
-	atomicState.oldValue = []						// atomic so we avoid calculations on multiple concurrent events
-    atomicState.oldTemp = 0
+  
+	subscribe(motions, "motion", modeHandler)
+    subscribe(contacts, "contact", modeHandler)
+    if(dimmers) { 
+		subscribe(dimmers, "switch.on", dimmerHandler)
+	}
+    if(ctbulbs) { 
+    	subscribe(ctbulbs, "switch.on", ctbulbHandler)
+    }
+    if(bulbs) { 
+    	subscribe(bulbs, "switch.on", bulbHandler)
+    }
+	subscribe(location, "mode", modeHandler)
 }
 
-def sunHandler(evt) {
-//	log.debug "$evt.name: $evt.value"
-	Integer maxK = 6500
-	Integer minK = 3000
-	Integer deltaK = maxK-minK
+def dimmerHandler(evt) { 
+	def hsb = getHSB()
+    for(dimmer in dimmers) { 
+        if(dimmer.currentValue("switch") == "on" && dimmer.currentValue("level") != hsb.b) {     
+        	log.debug "DIMMER2: ${hsb.b} "
+    		dimmer.setLevel(hsb.b)
+		}
+	}
+}
 
+def ctbulbHandler(evt) {
+	def hsb = getHSB()
+    def colorTemp = getCT()
+    for(ctbulb in ctbulb) { 
+        if(ctbulb.currentValue("switch") == "on") {
+        	if(ctbulb.currentValue("level") != hsb.b) { 
+    			ctbulb.setLevel(hsb.b)
+            }
+            ctbulb.setColorTemperature(colorTemp)
+		}
+	}
+    
+}
+
+def bulbHandler(evt) { 
+	def hsb = getHSB()
+	def newValue = [hue: hsb.h, saturation: hsb.s, level: hsb.b]
+    log.debug "updating ${evt.deviceId} with ${hsb}"
+    for(bulb in bulbs) {
+    	if(bulb.currentValue("switch") == "on") {
+			bulb.setColor(newValue)
+        }
+     }
+}
+
+
+
+// wait for bulbs to turn on
+def modeHandler(evt) {
+
+	def hsb = getHSB()
+    def colorTemp = getCT() 
+    for(dimmer in dimmers) {
+        if(dimmer.currentValue("switch") == "on" && dimmer.currentValue("level") != hsb.b) {     
+    		dimmer.setLevel(hsb.b)
+		}
+	}
+	def newValue = [hue: hsb.h, saturation: hsb.s, level: hsb.b]
+    for(bulb in bulbs) { 
+		if(bulb.currentValue("switch") == "on") { 
+			bulb.setColor(newValue) 
+		}
+	}
+	for(ctbulb in ctbulbs) {
+		if(ctbulb.currentValue("switch") == "on") { 
+        	if(ctbulb.currentValue("level") != hsb.b) { 
+        		ctbulb.setLevel(hsb.b)
+            }
+            ctbulb.setColorTemperature(colorTemp)
+		}
+	}
+}
+
+def getCT() { 
 	def after = getSunriseAndSunset()
 	def midDay = after.sunrise.time + ((after.sunset.time - after.sunrise.time) / 2)
 
 	def currentTime = now()
-//	log.debug "difference is $midDay : mode ${location.mode} :: $currentTime : ${location.mode}"
-
-    Integer temp
     
-	if(location.mode == "Night") { 
-//		log.info("this is starlight")
-		temp = maxK
-	}
-	else if(currentTime < after.sunrise.time) {
-//		log.info("this is early twilight")
-		temp = minK
+	def int colorTemp = 2700    
+	if(currentTime < after.sunrise.time) {
+		colorTemp = 2700
 	}
 	else if(currentTime > after.sunset.time) { 
-//		log.info("this is late twilight")
-		temp = minK
+		colorTemp = 2700
 	}
 	else {
 		if(currentTime < midDay) { 
-			temp = Math.round(minK + ((currentTime - after.sunrise.time) / (midDay - after.sunrise.time) * deltaK)) // we dont need the decimal precision here
-//			log.info("this is morning daylight: $temp")
+			colorTemp = 2700 + ((currentTime - after.sunrise.time) / (midDay - after.sunrise.time) * 3300)
 		}
 		else { 
-			temp = Math.round(maxK - ((currentTime - midDay) / (after.sunset.time - midDay) * deltaK))
-//			log.info("this is afternoon daylight: $temp")
+			colorTemp = 6000 - ((currentTime - midDay) / (after.sunset.time - midDay) * 3300)
+		}
+	}
+    
+    for (smode in smodes) {
+		if(location.mode == smode) { 	
+			colorTemp = 6000
+            if(dcamp == true) {
+            	colorTemp = 2700
+            }
+       	}
+	}
+    return colorTemp
+}
+
+def getHSB() {
+	def after = getSunriseAndSunset()
+	def midDay = after.sunrise.time + ((after.sunset.time - after.sunrise.time) / 2)
+
+	def currentTime = now()
+    
+    def brightness = 1
+	def int colorTemp = 2700    
+	if(currentTime < after.sunrise.time) {
+		colorTemp = 2700
+	}
+	else if(currentTime > after.sunset.time) { 
+		colorTemp = 2700
+	}
+	else {
+		if(currentTime < midDay) { 
+			colorTemp = 2700 + ((currentTime - after.sunrise.time) / (midDay - after.sunrise.time) * 3300)
+            brightness = ((currentTime - after.sunrise.time) / (midDay - after.sunrise.time))
+		}
+		else { 
+			colorTemp = 6000 - ((currentTime - midDay) / (after.sunset.time - midDay) * 3300)
+            brightness = 1 - ((currentTime - midDay) / (after.sunset.time - midDay))
 		}
 	}
 
-	if (temp == atomicState.oldTemp) return;			// avoid the redundant calculations for efficiency
-    atomicState.oldTemp = temp
+    if(dbright == false) { 
+    	brightness = 1
+    }
+    for (smode in smodes) {
+		if(location.mode == smode) { 	
+			log.debug("this is moonlight")
+            if(dcamp == true) { 
+            	colorTemp = 2700
+            }
+            else {
+				colorTemp = 6000
+            }
+			brightness = 0.01
+            last
+       	}
+	}
+
     
-    def hsb = rgbToHSB(ctToRGB(temp))
-	if (location.mode == "Night") hsb.b = 2;			// special for night sky simulation (could/should be bluer value perhaps
-    else hsb.b = Math.min( Math.round( ( ( temp - ( minK*0.8 ) ) / deltaK ) * 100 ) as Integer, 100);	// use a relative percentage of lightness (20%-->100%)
-    
- 	def newValue = [hue: Math.round(hsb.h) as Integer, saturation: Math.round(hsb.s) as Integer, level: Math.round(hsb.b) as Integer ?: 1]
-	if (newValue != atomicState.oldValue) {				// avoid updating all the lights unnecessarily for efficiency
-		atomicState.oldValue = newValue
-		log.info "Updated with daylight hueColor: $newValue"
-		bulbs?.setColor(newValue)
-	}   
+    def hsb = rgbToHSB(ctToRGB(colorTemp),brightness)
+    return hsb
 }
+    
 
 // Based on color temperature converter from 
 //  http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/
-// As commented, this will not work for color temperatures below 1900 or above 6600
-// Remove comments between *uncomment* lines
+// As commented, this will not work for color temperatures below 2700 or above 6000
 def ctToRGB(ct) { 
-	float r
-	float g
-	float b
-    
-//	if (ct < 2500) ct = 2500				// Philips Hue effective minimum (stretched)
-//	if (ct > 6500) ct = 6500				// Philips Hue effective maximum (stretched)
 
-// *uncomment below for full algorithm*
-//	if (ct < 1000) ct = 1000
-//	if (ct > 40000) ct = 40000
-	
+	if(ct < 2700) { ct = 2700 }
+    if(ct > 6000) { ct = 6000 }
+
 	ct = ct / 100
-//	if (ct <= 66 ) {
-		r = 255
-		g = 99.4708025861 * Math.log(ct) - 161.1195681661
-//	}
-//	else {
-//		r = 329.698727446 * Math.pow((ct-60), -0.1332047592)
-//		g = 288.1221695283 * Math.pow((ct-60), -0.0755148492)
-//	}
-//	if (r > 255) r = 255
-	if (g > 255) g = 255
-//	if (ct >= 66) {
-//		b = 255
-//	}
-//	else if (ct <= 19) {
-//		b = 0
-//	}
-//	else {
-		b = 138.5177312231 * Math.log(ct - 10) - 305.0447927307
-//	}
-	if (b > 255) b = 255
-// *uncomment above for full algorithm*
+	def r = 255
+	def g = 99.4708025861 * Math.log(ct) - 161.1195681661
+	def b = 138.5177312231 * Math.log(ct - 10) - 305.0447927307
 
-//	log.debug("raw-> r: $r g: $g b: $b")
-
-// Apply Hue gamma adjustment for more vibrant & accurate colors
-	float red = r/255
-	float green = g/255
-   	float blue = b/255    
-	red = ((red > 0.04045f) ? Math.pow((red + 0.055f) / (1.0f + 0.055f), 2.4f) : (red / 12.92f)) * 255
-	green = ((green > 0.04045f) ? Math.pow((green + 0.055f) / (1.0f + 0.055f), 2.4f) : (green / 12.92f)) * 255
-	blue = ((blue > 0.04045f) ? Math.pow((blue + 0.055f) / (1.0f + 0.055f), 2.4f) : (blue / 12.92f)) * 255
-
-//	log.debug("gamma-> r: $red g: $green b: $blue")
+//	log.debug("r: $r g: $g b: $b")
 
 	def rgb = [:]
-	rgb = [r: red, g: green, b: blue]
+	rgb = [r: r, g: g, b: b] 
 	rgb
 }
 
 // Based on color calculator from
-//  http://codeitdown.com/hsl-hsb-hsv-color/
-// 
-def rgbToHSB(rgb) {
+//  http://codeitdown.com/hsl-hsb-hsv-color/		 
+// Corrected brightness and saturation using calculator from
+//  http://www.rapidtables.com/convert/color/rgb-to-hsl.htm
+def rgbToHSB(rgb,brightness) {
 	def r = rgb.r
 	def g = rgb.g
 	def b = rgb.b
-	float hue, saturation, brightness;
+	float hue, saturation;
 
 	float cmax = (r > g) ? r : g;
 	if (b > cmax) cmax = b;
 	float cmin = (r < g) ? r : g;
 	if (b < cmin) cmin = b;
+
 	float delta = (cmax - cmin)
-
-	brightness = cmax / 255;
-    
-	if (cmax != 0) saturation = (cmax - cmin) / cmax;
-	else saturation = 0;
-
+	saturation = 0
+    if(delta != 0) saturation = delta / cmax
+		
 	if (saturation == 0) hue = 0;
 	else hue = 0.60 * ((g - b) / (255 -  cmin)) % 360
 
-//	log.debug("h: $hue s: $saturation b: $brightness")
+	log.debug("h: $hue s: $saturation b: $brightness")
  
 	def hsb = [:]    
-	hsb = [h: hue *100, s: saturation *100, b: brightness * 100]
+	hsb = [h: Math.round(hue * 100) as Integer, s: Math.round(saturation * 100) as Integer, b: Math.round(brightness * 100) as Integer ?: 100]
 	hsb
 }
-
-// Adapted above to return HSL level and saturation using calculator from
-//  http://www.rapidtables.com/convert/color/rgb-to-hsl.htm
-//
-//def rgbToHSL(rgb) {
-//	def r = rgb.r
-//	def g = rgb.g
-//	def b = rgb.b
-//	float hue, saturation, brightness;
-//
-//	float cmax = (r > g) ? r : g;
-//	if (b > cmax) cmax = b;
-//	float cmin = (r < g) ? r : g;
-//	if (b < cmin) cmin = b;
-//	float delta = (cmax - cmin)
-//
-//	brightness = ((cmax + cmin) / 2) / 255
-//	saturation = 0
-//	if (delta != 0)	saturation = (delta/255) / (1 - Math.abs((2*brightness)-1) )    	
-//
-//	if (saturation == 0) hue = 0;
-//	else hue = 0.60 * ((g - b) / (255 -  cmin)) % 360
-//
-//	log.debug("h: $hue s: $saturation b: $brightness")
-// 
-//	def hsb = [:]    
-//	hsb = [h: hue *100, s: saturation *100, b: brightness * 100]
-//	hsb
-//}
